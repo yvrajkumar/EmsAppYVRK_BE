b4b1ac8beb8a574a2e9590aad77fa717fc533d64
yvrajkumar <51850229+yvrajkumar@users.noreply.github.com>
2021-06-07 22:44:35 +0530

    Update CreateTable.rb

--- app/controllers/CreateTable/CreateTable.rb
+++ app/controllers/CreateTable/CreateTable.rb
@@ -9,7 +9,7 @@ module CreateTable
                 con = PG.connect :host => 'ec2-3-234-22-132.compute-1.amazonaws.com', :dbname => 'd1re1k2rplt7t2', :user => 'sewuvljklsqoqu', :password => '18f0134f286951799b72989aaf762d0ef926afad65d2c9e6f7c959aa991b1246'
                 exist = con.exec "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'emp')"
                 
-                if exist[0]["exists"]=='true' 
+                if exist[0]["exists"]=='t' 
                     con.exec "CREATE TABLE emp(Name VARCHAR(20), Email VARCHAR(30) PRIMARY KEY, Password VARCHAR(20), EmpCode VARCHAR(20), Address VARCHAR(50), JoiningDate  VARCHAR(20))"
                     puts "Table Created"
                 else
