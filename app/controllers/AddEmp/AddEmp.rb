6c01e74cf930bd78332ffd1a2e42e640e93ddcdd
yvrajkumar <51850229+yvrajkumar@users.noreply.github.com>
2021-06-07 16:43:31 +0530

    Update AddEmp.rb

--- app/controllers/AddEmp/AddEmp.rb
+++ app/controllers/AddEmp/AddEmp.rb
@@ -7,9 +7,9 @@ module AddEmp
         include ::Hanami::Action
         def call (env)
             puts "add emp"
-           # puts request.body.read()
-            req = request.body.read()
-            signupDetails = req
+           # puts request.body.read
+            req = request.body.read
+            signupDetails = JSON.parse(req)
             name = signupDetails['signupDetails']['name']
             email = signupDetails['signupDetails']['email']
             password = signupDetails['signupDetails']['password']
@@ -20,8 +20,8 @@ module AddEmp
                 con = PG.connect :host => 'ec2-3-234-22-132.compute-1.amazonaws.com', :dbname => 'd1re1k2rplt7t2', :user => 'sewuvljklsqoqu', :password => '18f0134f286951799b72989aaf762d0ef926afad65d2c9e6f7c959aa991b1246'
                 
                 existingUser = con.exec "select exists (select * from emp where email='#{email}')"
-                puts existingUser
-                if existingUser[0]["exists"]==='f' 
+                puts existingUser[0]["exists"]
+                if existingUser[0]["exists"]=='f' 
                     exist = con.exec "INSERT INTO emp values ('#{name}', '#{email}', '#{password}', '#{empcode}', '#{address}', '#{joiningdate}')"
                     puts "Emp Added"
                     result = "Account Created"
