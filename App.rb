abf09bf5ee259df19161196a0b9e7f6b0024f2c2
yvrajkumar <51850229+yvrajkumar@users.noreply.github.com>
2021-06-07 17:19:39 +0530

    Update App.rb

--- App.rb
+++ App.rb
@@ -5,7 +5,7 @@ class App
     Hanami::Router.new do
       get '/',        to: CreateTable::CreateTable
       post '/SignIn',   to: SignIn::SignIn
-      post '/SignUp', request.body.read
+      post '/SignUp', to: ->(env) { [200, {}, [request.body.read]] }
       post '/UpdateProfile', to: UpdateProfile::UpdateProfile
       post '/DeleteProfile', to: DeleteProfile::DeleteProfile
       post '/SendMail', to: SendMail::SendMail
